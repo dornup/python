5354cdc46970209d87363f40c8112c805ae47700
KromberK123 <a37507687@gmail.com>
2022-10-06 11:44:57 +0700

    эрджаст и элджаст классные !!!!

---
+++ lesson3/main.py
@@ -0,0 +1,49 @@
+# # многострочный string
+# x = "жираф\nжираф\nжираф"
+# x1 = """жираф 1
+# жираф 2
+# жираф 3"""
+# print(x1)
+#списки
+# name_1 = "жираф"
+# name_2 = "слоник"
+# name_3 = "львенок"
+# brartva = [name_1, name_2, name_3]
+# print(brartva)
+# print(brartva[0]) #вывела жирафа
+# print(brartva[-3]) #вывела жирафа
+# alphabet = "ABCDEFG"
+# print(alphabet[0]) #хотелка а
+# print(alphabet[-1]) # хотелка г
+# print(alphabet [0] + alphabet [1] + alphabet [2]) # сложнаааа
+# print(alphabet[0:3]) # простаааа
+# print(alphabet[:3]) # то же, что и сферху
+# print(alphabet[-3:]) # конец
+# print(alphabet[:6:2]) # вывод кузнечиком (вторая цифра обозначает цену шага)
+# print(alphabet[::-1]) #обратный порядок
+# print(alphabet[::-2]) #обраточка через одного
+#
+
+# print("первая буква" , x[0])
+# x = input("введи словечко:")
+# # temp = x[-1]#последний символ
+# # print(x.index(temp)+1) #индекс последнего символа +1
+# print(len(x)) # лена сама посчитает
+
+# print("имя файла:", path[-10:])
+# print("расширение:", path[-3:])
+# print("имя каталога:", path [3:10])
+# print("полный путь к каталогу:", path[:10]) # не круто(((
+# path = "C:/Python3/python.exe"
+# temp = path.split("/")
+# print(temp)
+# print("имя файла:", temp[-1])
+# print("расширение:",temp[-1][-3:])
+# print("имя каталога:",temp[1])
+# print("полный путь к каталогу:",temp[0] + "/" + temp[1])
+# chapter_1 = input("Глава 1:")
+# page_1 = input("Страница:")
+# chapter_2 = input("Глава 2:")
+# page_2 = input("Страница:")
+# print(chapter_1.rjust(15), page_1.rjust(15))#эржаст - мой брат, он выравнивает строчки
+# print(chapter_2.rjust(15), page_2.rjust(15))# элджаст - брат эрджаста, он тоже крутой чел
