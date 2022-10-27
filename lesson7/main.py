45e28fafc890de0616e952f014279e147b79a331
movavi_school <Tanahu2010@gmail.com>
2022-10-27 11:58:50 +0700

    я учу путхон))

---
+++ lesson7/main.py
@@ -0,0 +1,54 @@
+# # #ZeroDivisionError
+# # #x = 5/0
+# # #TypeError:
+# # #x = "a" + 15
+# # IndexError:
+# # x=[0, -15, "Aнтон"]
+# # x[3]
+# SyntaxError:
+# x = "Привет, я Антон
+# NameError:
+# x = "Я строчка"
+# print(x2)
+# # assert --> AssertionError
+# def summa (numbers)
+#     return sum(numbers)
+#
+# print(summa([3,4]))
+#
+# assert summa([1,2]) == 3
+# assert summa([1,2]) == 4
+# # try - пытаться
+# # except - ловить ошибку
+# #finally - в конце
+# try:
+#     number = int(input())
+#     x = 5/ number
+# except ZeroDivisionError:
+#     print("Слышь, нельзя на ноль делитб!")
+# except ZeroDivisionError:
+#     print("Хочу кифир")
+# except Exception:
+#     print("Не трай((")
+# else: #когда все чикипуки
+#     print("Я поделив)")
+# finally:
+#     print("всьо")
+#
+#
+# print("я закончил работать")
+# # PASS
+# try:
+#     number = int(input())
+#     x = 5/number
+# except Exception:
+#     pass # ЗАВАЛИСЬ
+# if 5 == 5:
+#     pass #TODO:
+try:
+    x = input("Введи имя:")
+    if x == "Антон":
+        raise Exception ("Не отдаем Антона!")# Вызвать исключение
+except Exception as error_message:
+    print("ЗАПРЕЩЕНКА", error_message)
+
