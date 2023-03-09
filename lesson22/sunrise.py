6287b540e0159c13555b0b07ff1fea718f08586d
Melany Dorn <114129838+dornup@users.noreply.github.com>
2023-03-09 07:26:46 +0700

    Add files via upload
    ааааа ну я пыталась...

---
+++ lesson22/sunrise.py
@@ -0,0 +1,13 @@
+import requests
+import geopy
+from geopy.geocoders import Nominatim
+from pprint import pprint
+import time
+
+pupipu = Nominatim(user_agent = "Melanya")
+locatia = pupipu.geocode("Russia, Novosibirsk").raw
+pprint(locatia)
+response = requests.get(https://api.sunrise-sunset.org/json?lat=locatia["lat"]&lng=locatia["lon"]& date = today)
+print(response["sunrise"],response["sunset"])
+
+
