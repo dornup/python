f59656334d5e46d983c995cd80d594471267b7e9
Dornup <mdiscool1@mail.ru>
2023-07-22 12:29:01 +0700

    работа на каникулах

--- grokaem_alg/dinamic_programming.py
+++ grokaem_alg/dinamic_programming.py
@@ -1,25 +1,25 @@
 # динамическое программирование
 
-weight = {1: 1,
+weight = {1: 1,  # вес предметов
           2: 4,
           3: 3}
-price = {1: 1500,
+price = {1: 1500,  # цена предметов
          2: 3000,
          3: 2000}
-place = {1: "guitar",
+place = {1: "guitar",  # местоположение предметов в таблицах
          2: "recorder",
          3: "notebook"}
 
-elements = {0: {0: "nothing",
-                1: "nothing",
-                2: "nothing",
-                3: "nothing",
-                4: "nothing"},
+elements = {0: {0: [None],  # предметы, которые мы берем в каждом подрюкзаке
+                1: [None],  # нулевая строка и столбец нужны, чтобы не сломать программу
+                2: [None],
+                3: [None],
+                4: [None]},
             1: {},
             2: {},
             3: {}}
 
-table = {0: {0: 0,
+table = {0: {0: 0,  # таблица цен в каждом подрюкзаке
              1: 0,
              2: 0,
              3: 0,
@@ -40,17 +40,21 @@ table = {0: {0: 0,
              3: 0,
              4: 0}}
 
-stolen = set()
+# заполнение таблицы
+for i in range(1, len(table)):  # проходимся по каждому предмету (строка)
+    for k in table[i]:  # проходимся по каждому подрюкзаку (столбец)
+        el = []  # список взятых элементов
+        if k >= weight[i]:  # если предмет влезает
+            if table[i - 1][k] < price[i] + table[i - 1][k - weight[i]]:  # если цена предмета + цена оставшегося пространства больше предыдущего максимума
+                table[i][k] = price[i] + table[i - 1][k - weight[i]]  # заносим цену пространства в таблицу
+                el = [i for i in elements[i-1][k-weight[i]] if i]  # добавляем все взятые предметы в список
+                el.append(place[i])
+                elements[i][k] = el  # заносим данные в таблицу
+                continue  # идем на следующую клетку
+        # ИНАЧЕ (сработает, если мы не заходили хотя бы в одно предыдущее условие)
+        table[i][k] = table[i - 1][k]  # максимум сохраняется
+        elements[i][k] = elements[i - 1][k]  # элементы сохраняются
 
-for i in range(1, len(table)):
-    for k in table[i]:
-        if k >= weight[i]:
-            print(k - weight[i])
-            if table[i - 1][k] < price[i] + table[i - 1][k - weight[i]]:
-                table[i][k] = price[i] + table[i - 1][k - weight[i]]
-                elements[i][k] = [place[i], elements[i - 1][k - weight[i]]]
-                continue
-        table[i][k] = table[i - 1][k]
-        elements[i][k] = elements[i - 1][k]
-    print(table)
-    print(elements)
+# выводим данные из последних клеток таблицы
+print(f"Нужно украсть: {elements[max(list(elements.keys()))][max(list(elements[i].keys()))]}")
+print(f"Стоимость: {table[max(list(table.keys()))][max(list(table[i].keys()))]}")
