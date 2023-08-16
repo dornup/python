9a34af1737edaa72d129bc647d36211ac76145d6
Dornup <mdiscool1@mail.ru>
2023-08-17 00:33:53 +0700

    я сюда еще вернусь завтра

---
+++ around_the_weather_bot/main.py
@@ -0,0 +1,10 @@
+# импортируем всякие нужные библиотеки
+import os
+import datetime
+import requests
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+bot = Bot(token='6439271902:AAHWCTBiDahbVSj6nzRHn-RVMcrs42uZnT0')
+dispatcher = Dispatcher(bot)
