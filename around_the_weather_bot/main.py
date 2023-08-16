5f81300351a3c1963dbb870d6023aacc453bc351
Melany Dorn <114129838+dornup@users.noreply.github.com>
2023-08-17 01:00:17 +0700

    Update main.py

--- around_the_weather_bot/main.py
+++ around_the_weather_bot/main.py
@@ -6,5 +6,5 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-bot = Bot(token='6439271902:AAHWCTBiDahbVSj6nzRHn-RVMcrs42uZnT0')
+bot = Bot(token='Token')
 dispatcher = Dispatcher(bot)
