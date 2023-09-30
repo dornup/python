3638337bf7c698884023ebeaa154ad6ae10f3282
Dornup <mdiscool1@mail.ru>
2023-09-30 23:14:36 +0700

    я сюда еще вернусь завтра

--- timer.py
+++ timer.py
@@ -6,6 +6,14 @@ start = time.perf_counter()
 
 # code here
 
+a = int(input())
+b = int(input())
+for i in str(b):
+    if int(i) >= a:
+        print('НЕТ')
+        quit()
+print('ДА')
+
 end = time.perf_counter()
 print(f"time = {end - start:0.4f}")
 
