636daca230015f8301c90075a8cff589bfd91781
Dornup <mdiscool1@mail.ru>
2023-11-11 00:36:32 +0700

    я сюда еще вернусь завтра

--- noviy_mir/ratings.py
+++ noviy_mir/ratings.py
@@ -1,19 +1,35 @@
-# input:
-n = int(input('Кол-во оценок: '))
-end = float(input('Необходимый рейтинг: '))
-rate = float(input('Рейтинг сейчас: '))
-min_sum_rates = round((rate-0.05)*n, 1)
-max_sum_rates = round((rate+0.04)*n, 1)
-max_five, min_five = 0, 0
-print(min_sum_rates, max_sum_rates)
+import gspread
 
-while rate < end:
-    if max_sum_rates/n < end:
-        max_sum_rates += 5
-        max_five += 1
-    n += 1
-    min_sum_rates += 5
-    min_five += 1
-    rate = round(min_sum_rates/n, 1)
+gs = gspread.service_account(filename='new-world-404713-3ea0feb6f959.json')
+sh = gs.open_by_key('1zAUex3RlqXE4J3ew4bZXRCOI7roWoO3Oe-iXxfIYAlU')
 
-print(f'Нужно от {min_five} до {max_five} пятерок c какой-то погрешностью')
+sheet = sh.sheet1.get_all_values()
+rates = [i.replace(',', '.') for i in sh.sheet1.col_values(12)[2:]]
+ns = sh.sheet1.col_values(13)[2:]
+
+
+for i in range(len(ns)):
+
+    #n = int(sh.sheet1.acell(f'M{i+1}').value)
+    n = int(ns[i])
+    end = 4.8
+    rate = float(rates[i])
+    min_sum_rates = round((rate-0.05)*n, 1)
+    max_sum_rates = round((rate+0.04)*n, 1)
+    max_five, min_five = 0, 0
+    print(i, rate, n)
+
+    if rate == 0.0:
+        sh.sheet1.update_acell(f'P{i+3}', '1')
+        continue
+
+    while rate < end:
+        if max_sum_rates/n < end:
+            max_sum_rates += 5
+            max_five += 1
+        n += 1
+        min_sum_rates += 5
+        min_five += 1
+        rate = round(min_sum_rates/n, 1)
+
+    sh.sheet1.update_acell(f'P{i+3}', str(max_five))
