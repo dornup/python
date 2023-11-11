6a5578dc155a7f99f86ad27319af186a220483d4
Melany Dorn <114129838+dornup@users.noreply.github.com>
2023-11-11 10:13:21 +0700

    Update ratings.py

--- noviy_mir/ratings.py
+++ noviy_mir/ratings.py
@@ -1,7 +1,7 @@
 import gspread
 
-gs = gspread.service_account(filename='new-world-404713-3ea0feb6f959.json')
-sh = gs.open_by_key('1zAUex3RlqXE4J3ew4bZXRCOI7roWoO3Oe-iXxfIYAlU')
+gs = gspread.service_account(filename='your json file')
+sh = gs.open_by_key('your key')
 
 sheet = sh.sheet1.get_all_values()
 rates = [i.replace(',', '.') for i in sh.sheet1.col_values(12)[2:]]
